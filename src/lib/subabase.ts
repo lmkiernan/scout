9223d512e529765d36c503468f0abe23d8d0f6c0
lmkiernan <liamkiernan03@gmail.com>
2025-05-24 17:43:31 -0400

    signup fully working! Async storage added

--- src/lib/subabase.ts
+++ src/lib/subabase.ts
@@ -1,7 +1,27 @@
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 console.log('SUPABASE_URL:', SUPABASE_URL);
 console.log('SUPABASE_ANON_KEY:', SUPABASE_ANON_KEY);
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY,
+    {
+      auth: {
+        // use AsyncStorage in place of localStorage
+        storage: AsyncStorage,
+  
+        // auto-persist sessions to storage
+        persistSession: true,
+  
+        // auto-refresh your token when it’s about to expire
+        autoRefreshToken: true,
+  
+        // React Native has no URL to detect, so disable
+        detectSessionInUrl: false,
+      }
+    }
+  );
